79cbf4a11c6617f06e4e84d846522cebb86fa65f
Younghyun Lee <younghyunlee22@gmail.com>
2023-11-28 17:51:41 -0600

    testing with a different string

--- script.js
+++ script.js
@@ -9,17 +9,17 @@ If a number is divisible by both 3 and 5, log “Fizz Buzz.”
 If a number is not divisible by either 3 or 5, log the number.
 */
 
-// for (let i = 0; i <= 100; i++) {
-//   if (i % 3 === 0 && i % 5 === 0) {
-//     console.log("Fizz Buzz");
-//   } else if (i % 3 === 0) {
-//     console.log("Fizz");
-//   } else if (i % 5 === 0) {
-//     console.log("Buzz");
-//   } else {
-//     console.log(i);
-//   }
-// }
+for (let i = 0; i <= 100; i++) {
+  if (i % 3 === 0 && i % 5 === 0) {
+    console.log("Fizz Buzz");
+  } else if (i % 3 === 0) {
+    console.log("Fizz");
+  } else if (i % 5 === 0) {
+    console.log("Buzz");
+  } else {
+    console.log(i);
+  }
+}
 
 /**
  * Part 2: Prime Time
@@ -33,25 +33,26 @@ Continuing with the example above, if n is equal to 4, your loop should log 5. S
 Be careful! If you set n to a number too large, your loop could take a long time to process.
  */
 
-// let x = 23;
+let x = 23;
 
-// while (true) {
-//   let isPrime = true;
-//   x++;
-//   for (let i = 2; i < x; i++) {
-//     if (x % i === 0) {
-//       isPrime = false;
-//       break;
-//     }
-//   }
-//   if (isPrime) {
-//     console.log(x);
-//     break;
-//   }
-// }
+while (true) {
+  let isPrime = true;
+  x++;
+  for (let i = 2; i < x; i++) {
+    if (x % i === 0) {
+      isPrime = false;
+      break;
+    }
+  }
+  if (isPrime) {
+    console.log(x);
+    break;
+  }
+}
 
 /**
- * Your task is to write a script that accomplishes the following:
+ * Part 3: Feeling Loopy
+Your task is to write a script that accomplishes the following:
 Loop through the characters of a given CSV string.
 Store each “cell” of data in a variable.
 When you encounter a comma, move to the next cell.
@@ -105,7 +106,7 @@ for (let i = 0; i < string.length; i++) {
 }
 
 console.log("=====Method 2=====");
-const csvString = `ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26`;
+const csvString = `Index,Mass (kg),Spring 1 (m),Spring 2 (m)\n1,0.00,0.050,0.050\n2,0.49,0.066,0.066\n3,0.98,0.087,0.080\n4,1.47,0.116,0.108\n5,1.96,0.142,0.138\n6,2.45,0.166,0.158\n7,2.94,0.193,0.174\n8,3.43,0.204,0.192\n9,3.92,0.226,0.205\n10,4.41,0.238,0.232`;
 
 let cellData = "";
 let rowData = "";
